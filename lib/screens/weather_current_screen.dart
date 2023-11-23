df55ed4427e67e8b4ecad78fd4a76b0206f5b8e6
LautaroMoor <lautimoor2@gmail.com>
2023-11-22 21:47:13 -0300

    Trabajo terminado hardcodeado

--- lib/screens/weather_current_screen.dart
+++ lib/screens/weather_current_screen.dart
@@ -10,13 +10,46 @@ class WeatherCurrentScreen extends StatefulWidget {
 }
 
 class _WeatherCurrentScreenState extends State<WeatherCurrentScreen> {
+  Map<String, dynamic> currentWeatherData = {};
+
+  @override
+  void initState() {
+    super.initState();
+
+    currentWeatherData = {
+      "currentWeather": {
+        "city": "Estado de Bahia",
+        "temperature": 21.03,
+        "weatherDescription": "nubes"
+      }
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
+    var temperature = currentWeatherData['currentWeather']['temperature'];
+    var description =
+        currentWeatherData['currentWeather']['weatherDescription'];
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Clima Actual - ${widget.city}'),
       ),
-      body: Container(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Temperatura: $temperature°C',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Descripción: $description',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
